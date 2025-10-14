30b1d0e2a8e2c91a5caf93e973113c4de8e3984e
gromfy01 <georgy.romanenco@gmail.com>
2025-10-14 19:07:04 +0000

    HW2: YARN cluster (Ansible + docs)

---
+++ hw2/ansible/roles/hadoop/files/profile.d/hadoop.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-11-openjdk-amd64}
+export HADOOP_HOME=${HADOOP_HOME:-/opt/hadoop}
+export HADOOP_CONF_DIR=/etc/hadoop
+export PATH=$PATH:/opt/hadoop/bin:/opt/hadoop/sbin
